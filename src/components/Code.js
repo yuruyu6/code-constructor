7be90b4a0a1ec67f6ce9dbd4e3bcfaa0fec41ab6
yuruyu6 <yuruyu6@gmail.com>
2022-11-02 16:39:33 +0200

    optime styles for code block;

--- src/components/Code.js
+++ src/components/Code.js
@@ -4,36 +4,42 @@ import theme from 'prism-react-renderer/themes/github'
 import { Box } from '@mui/material'
 
 const Code = ({ code }) => (
-  <Box sx={{maxHeight: '920px', overflow: 'auto'}}>
-  <Highlight {...defaultProps} theme={theme} code={code} language="cpp">
-    {({ className, style, tokens, getLineProps, getTokenProps }) => (
-      <Box sx={{overflow: 'auto'}} component="pre" className={className} style={style}>
-        {tokens.map((line, i) => (
-          <Box
-            sx={{ display: 'table-row' }}
-            key={i}
-            {...getLineProps({ line, key: i })}
-          >
-            <Box
-              sx={{
-                display: 'table-cell',
-                textAlign: 'right',
-                userSelect: 'none',
-              }}
-              pr={1}
+  <Box sx={{ maxHeight: '920px', overflow: 'auto' }}>
+    <Highlight {...defaultProps} theme={theme} code={code} language="cpp">
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <Box
+          sx={{ overflow: 'auto' }}
+          component="pre"
+          className={className}
+          style={style}
+        >
+          {tokens.map((line, i) => (
+            <div
+              style={{ display: 'table-row' }}
+              key={i}
+              {...getLineProps({ line, key: i })}
             >
-              {i + 1}
-            </Box>
-            <Box sx={{ display: 'table-cell' }}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </Box>
-          </Box>
-        ))}
-      </Box>
-    )}
-  </Highlight></Box>
+              <p
+                style={{
+                  display: 'table-cell',
+                  textAlign: 'right',
+                  userSelect: 'none',
+                }}
+                pr={1}
+              >
+                {i + 1}
+              </p>
+              <p style={{ display: 'table-cell' }}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </p>
+            </div>
+          ))}
+        </Box>
+      )}
+    </Highlight>
+  </Box>
 )
 
 export default Code
